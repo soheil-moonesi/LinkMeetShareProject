2fcf99ad62da6d7bb8803f58e9a7a642131186ce
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2025-04-27 18:03:28 +0330

    User , MeetingLink

---
+++ LinkMeetShareProject/Models/MeetingLink.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkMeetShareProject.Models
+{
+    public class MeetingLink
+    {
+        public int id { get; set; }
+        public string Tittle { get; set; }
+        public string Link { get; set; }
+        public DateTime Time { get; set; }
+
+    }
+}
