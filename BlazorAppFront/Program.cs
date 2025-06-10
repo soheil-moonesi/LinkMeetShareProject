3207a5545f6e837012f8247489f18af597f17d4e
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2025-06-10 15:50:36 +0330

    jwt login done

--- BlazorAppFront/Program.cs
+++ BlazorAppFront/Program.cs
@@ -1,10 +1,18 @@
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Blazored.LocalStorage;
 using BlazorAppFront;
 using BlazorAppFront.Services;
 using FluentValidation;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 using static BlazorAppFront.Pages.Home;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -17,5 +25,10 @@ builder.Services.AddHttpClient("ApiCalls");
 builder.Services.AddScoped<UserServices>();
 builder.Services.AddRadzenComponents();
 builder.Services.AddTransient<IValidator<Model>, UserValidator>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomStateProvider>();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddBlazoredLocalStorage();
+
 
 await builder.Build().RunAsync();
